892baaa86de2a1f319a24cfb5530f9c52e109f29
JideonowFelixx <jideonwofoc@gmail.com>
2023-01-05 09:49:04 +0100

    added working example for firebase query

---
+++ src/config/FirebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBT4bKBXnPzq5BzJQAtAbAfQYWcXKk5ziw",
+    authDomain: "e-commerce-project-797ff.firebaseapp.com",
+    projectId: "e-commerce-project-797ff",
+    storageBucket: "e-commerce-project-797ff.appspot.com",
+    messagingSenderId: "786618161724",
+    appId: "1:786618161724:web:da4d899093990d7c68c139",
+    measurementId: "G-570JJ5E5M9"
+};
---
+++ src/examples/firebase_query.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+import { initializeApp } from 'firebase/app';
+import {firebaseConfig} from '../config/FirebaseConfig';
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
+
+function FirebaseQueryExample() {
+
+  const [Data, setData] = useState([]);
+  // var Data =[];
+
+  
+  
+  // FirebaseQuery('products');
+  useEffect(()=>{
+        const app = initializeApp(firebaseConfig);
+        const db = getFirestore(app);
+        async function FirebaseQuery(query){
+            const queryCol = collection(db, query);
+            const querySnapshot = await getDocs(queryCol);
+            const query_data = querySnapshot.docs.map(doc => doc.data());
+            setData(query_data);
+            // Data = [...query_data]
+            console.log('inside fetch', Data)
+        }
+        FirebaseQuery('products');
+    }, [Data])
+
+
+
+  console.log('datax', Data)
+
+  return (
+    <ul className="list">
+        {Data.map(e=>(
+            <li key={Data.indexOf(e)}>
+                {e.name}
+            </li>
+        ))}
+    </ul>
+  );
+}
+
+export default FirebaseQueryExample;
