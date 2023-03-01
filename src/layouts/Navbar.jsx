e7e43385bb299b02c552ec641086bf33ca6b203b
Samuel Jacobs <44222526+Samuel-jacobs@users.noreply.github.com>
2023-03-01 16:21:51 +0100

    made navbar responsive

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,23 +1,39 @@
-import React from 'react';
+// import React from 'react';
 import { Link } from "react-router-dom";
+import React, {useState} from 'react';
 // import '/navbar.css';
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const navbarToggle = () => {
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <div className="navbar">
-      <Link to={`/`} > Supro</Link>
-      <div className="navlinks">
-        <Link to={`/`} className={'navlink'}> Categories</Link>
-        <Link to={`/test-page1`} className={'navlink'}>Whats New</Link>
-        <Link to={`/test-page2`} className={'navlink'}>Deals</Link>
-        <Link to={`/test-page3`} className={'navlink'}>Delivery</Link>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-logo">
+          <Link to={`/`} className="logo"> Supro</Link>
+        </div>
+        <div className={`navbar-menu ${isOpen ? "open" : "close"}`}>
+          <div className="nav-links">
+            <Link to={`/test-page1`} className={'nav-item'}> Categories</Link>
+            <Link to={`/test-page1`} className={'nav-item'}>Whats New</Link>
+            <Link to={`/test-page2`} className={'nav-item'}>Deals</Link>
+            <Link to={`/test-page3`} className={'nav-item'}>Delivery</Link>
+          </div>
+          <div className="navbtns">
+            <input type="text" placeholder='search products' />
+            <button>user</button>
+            <button>cart</button>
+          </div>
+        </div>
+        <button className="navbar-toggle" onClick={navbarToggle}>
+            <span className="navbar-toggle--icon">Open</span>
+          </button>
       </div>
-      <div className='navbtns'>
-        <input type="text" placeholder='search products' />
-        <button>user</button>
-        <button>cart</button>
-      </div>
-    </div>
+    </nav>
   )
 }
 
